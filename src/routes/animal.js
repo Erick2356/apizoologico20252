d8362f1403e3725ef70208a4bcedd9439078f2a7
Hey <hey@hotmail.com>
2025-09-24 16:19:44 -0500

    API

---
+++ src/routes/animal.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const animalSchema = require("../models/animalModel");
+
+router.post("/animals", (req, res) => {
+  const animal = animalSchema(req.body);
+  animal
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+module.exports = router;

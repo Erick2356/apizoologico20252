c13d30f843a0524e06b3d275e612617d83a60b42
Erick <erick.pmartinez@hotmail.com>
2025-11-05 16:29:49 -0500

    cambio

--- src/models/animalModel.js
+++ src/models/animalModel.js
@@ -14,7 +14,7 @@ const animalSchema = mongoose.Schema({
     },
     fecha: {
         type: Date,
-        required: true,
+        required: false,
     }
 });
 module.exports = mongoose.model("Animal", animalSchema);
